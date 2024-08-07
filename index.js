bbed0c6775b48142b62a16a48d21829a9494cfcd
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2024-08-07 20:43:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -90,6 +90,23 @@ client.on(Events.MessageCreate, async message => {
 			message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 			break;
 
+		case "mp4" :
+			let vidURL = args[0];
+			var vidPattern = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+			if(vidPattern.test(vidURL) === true) {
+				const res = await axios.get(`https://skizo.tech/api/download?apikey=confutatis3000&url=${vidURL}`);
+				message.reply({
+					files: [{
+						attachment: res.data.response.formats[0].url,
+						name: 'donglot.mp4',
+					}]
+				});
+			} else if(vidPattern.test(vidURL) === false) {
+				message.reply('yg bener url nya pepej');
+			}
+			break;
+
 		case "avatar" :
 			let mention = client.users.cache.get(args[0]) || message.mentions.users.first() || message.author 
   			let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
