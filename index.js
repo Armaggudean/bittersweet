a1db3e508af17d06759417dda8d56f1ae0e8bb6c
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2024-08-08 20:09:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ client.on('messageCreate', async (message) => {
 		message.channel.id !== "1270972186857050217" && //heso
 		message.channel.id !== "1270930331645644800" &&//1270930331645644800 AWP
 		message.channel.id !== "1270985458565775504" && //chillin
-		message.channel.id !== "1271004304345534534" //pii
+		message.channel.id !== "1271004304345534534" &&//pii
+		message.channel.id !== "1267390785725403238" //shiro
 	) return;
 	if(
 		message.author.id === "444052085927837720"
